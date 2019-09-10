485047efcc8a62a514aa88d0aeeea33b0e3b082d
sunshicheng <1248185022@qq.com>
2019-09-10 21:09:59 +0800

    up first

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python_scrapy
+
+学习python数据采集，跟着书来敲。
---
+++ gitconfig.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("git config --local user.name 'sunshicheng'")
+os.system("git config --local user.email '1248185022@qq.com'")
+os.system("git config --local credential.helper store")
